194a7408a86168f67dfe8c0c3245f19c34fb0213
roopak <roopak.houji@truminds.com>
2026-04-02 15:13:27 +0530

    Minior chnages in the folder strcuture and script file names

--- Docs/Script_files/build-modules/PC/build-image.sh
+++
@@ -1,3 +0,0 @@
-# copy this files in this path - /home/roopak/Documents/iq9/build-qcom-wayland/
-# This will generate an image which can be flashed using pact by connecting device in QDL mode
-bitbake qcom-multimedia-image -C compile 
