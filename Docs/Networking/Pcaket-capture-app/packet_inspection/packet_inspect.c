cc7bd8d05e4349053f44b406e28c3313640c7e1c
roopak <roopak.houji@truminds.com>
2026-05-13 12:34:31 +0530

    Included no:of packets received in the packet inspection code

--- Docs/Networking/Pcaket-capture-app/packet_inspection/packet_inspect.c
+++ Docs/Networking/Pcaket-capture-app/packet_inspection/packet_inspect.c
@@ -1,5 +1,3 @@
-// compile - gcc packet_sniffer.c -o packet_sniffer
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -11,109 +9,122 @@
 #include <netinet/tcp.h>
 #include <netinet/udp.h>
 #include <net/ethernet.h>
+#include <signal.h>
 
 #define BUFFER_SIZE 65536
 
+static unsigned long packet_count = 0;
+
+void stop_capture(int signo)
+{
+    printf("\n\nCapture stopped.\n");
+    printf("Total packets received = %lu\n", packet_count);
+    exit(0);
+}
+
 void inspect_packet(unsigned char *buffer, int size)
 {
-   struct ethhdr *eth = (struct ethhdr *)buffer;
+    struct ethhdr *eth = (struct ethhdr *)buffer;
 
-   printf("\n==== New Packet ====\n");
+    printf("\n==== Packet #%lu ====\n", packet_count);
 
-   // L2 - Ethernet
-   printf("L2 Ethernet Header\n");
-   printf("Source MAC: %02x:%02x:%02x:%02x:%02x:%02x\n",
-          eth->h_source[0], eth->h_source[1], eth->h_source[2],
-          eth->h_source[3], eth->h_source[4], eth->h_source[5]);
+    // L2 - Ethernet
+    printf("L2 Ethernet Header\n");
 
-   printf("Destination MAC: %02x:%02x:%02x:%02x:%02x:%02x\n",
-          eth->h_dest[0], eth->h_dest[1], eth->h_dest[2],
-          eth->h_dest[3], eth->h_dest[4], eth->h_dest[5]);
+    printf("Source MAC: %02x:%02x:%02x:%02x:%02x:%02x\n",
+           eth->h_source[0], eth->h_source[1], eth->h_source[2],
+           eth->h_source[3], eth->h_source[4], eth->h_source[5]);
 
-   // Check if IP
-   if (ntohs(eth->h_proto) == ETH_P_IP)
-   {
-       struct iphdr *ip = (struct iphdr *)(buffer + sizeof(struct ethhdr));
+    printf("Destination MAC: %02x:%02x:%02x:%02x:%02x:%02x\n",
+           eth->h_dest[0], eth->h_dest[1], eth->h_dest[2],
+           eth->h_dest[3], eth->h_dest[4], eth->h_dest[5]);
 
-       struct sockaddr_in src, dest;
-       src.sin_addr.s_addr = ip->saddr;
-       dest.sin_addr.s_addr = ip->daddr;
+    // Check if IP
+    if (ntohs(eth->h_proto) == ETH_P_IP)
+    {
+        struct iphdr *ip = (struct iphdr *)(buffer + sizeof(struct ethhdr));
 
-       printf("\nL3 IP Header\n");
-       printf("Source IP: %s\n", inet_ntoa(src.sin_addr));
-       printf("Destination IP: %s\n", inet_ntoa(dest.sin_addr));
-       printf("Protocol: %d\n", ip->protocol);
+        struct sockaddr_in src, dest;
+        src.sin_addr.s_addr = ip->saddr;
+        dest.sin_addr.s_addr = ip->daddr;
 
-       // L4
-       if (ip->protocol == IPPROTO_TCP)
-       {
-           struct tcphdr *tcp = (struct tcphdr *)(buffer +
-                                   sizeof(struct ethhdr) +
-                                   ip->ihl * 4);
+        printf("\nL3 IP Header\n");
+        printf("Source IP: %s\n", inet_ntoa(src.sin_addr));
+        printf("Destination IP: %s\n", inet_ntoa(dest.sin_addr));
+        printf("Protocol: %d\n", ip->protocol);
 
-           printf("\nL4 TCP Header\n");
-           printf("Source Port: %u\n", ntohs(tcp->source));
-           printf("Destination Port: %u\n", ntohs(tcp->dest));
-           printf("SYN: %d ACK: %d\n", tcp->syn, tcp->ack);
+        // TCP
+        if (ip->protocol == IPPROTO_TCP)
+        {
+            struct tcphdr *tcp = (struct tcphdr *)(buffer +
+                                    sizeof(struct ethhdr) +
+                                    ip->ihl * 4);
 
-           unsigned char *payload = buffer +
-               sizeof(struct ethhdr) +
-               ip->ihl * 4 +
-               tcp->doff * 4;
+            printf("\nL4 TCP Header\n");
+            printf("Source Port: %u\n", ntohs(tcp->source));
+            printf("Destination Port: %u\n", ntohs(tcp->dest));
+            printf("SYN: %d ACK: %d\n", tcp->syn, tcp->ack);
+        }
 
-           int payload_size = size -
-               (sizeof(struct ethhdr) +
-                ip->ihl * 4 +
-                tcp->doff * 4);
+        // UDP
+        else if (ip->protocol == IPPROTO_UDP)
+        {
+            struct udphdr *udp = (struct udphdr *)(buffer +
+                                    sizeof(struct ethhdr) +
+                                    ip->ihl * 4);
 
-           if (payload_size > 0)
-           {
-               printf("\nL7 Payload (first 20 bytes):\n");
-               for (int i = 0; i < payload_size && i < 20; i++)
-                   printf("%02x ", payload[i]);
-               printf("\n");
-           }
-       }
-       else if (ip->protocol == IPPROTO_UDP)
-       {
-           struct udphdr *udp = (struct udphdr *)(buffer +
-                                   sizeof(struct ethhdr) +
-                                   ip->ihl * 4);
-
-           printf("\nL4 UDP Header\n");
-           printf("Source Port: %u\n", ntohs(udp->source));
-           printf("Destination Port: %u\n", ntohs(udp->dest));
-       }
-   }
+            printf("\nL4 UDP Header\n");
+            printf("Source Port: %u\n", ntohs(udp->source));
+            printf("Destination Port: %u\n", ntohs(udp->dest));
+        }
+    }
 }
 
 int main()
 {
-   int sock_raw;
-   unsigned char *buffer = (unsigned char *)malloc(BUFFER_SIZE);
+    int sock_raw;
 
-   sock_raw = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
-   if (sock_raw < 0)
-   {
-       perror("Socket Error");
-       return 1;
-   }
+    unsigned char *buffer =
+        (unsigned char *)malloc(BUFFER_SIZE);
 
-   printf("Listening for packets...\n");
+    signal(SIGINT, stop_capture);
 
-   while (1)
-   {
-       int data_size = recvfrom(sock_raw, buffer, BUFFER_SIZE, 0, NULL, NULL);
-       if (data_size < 0)
-       {
-           perror("Recv Error");
-           return 1;
-       }
+    sock_raw = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
 
-       inspect_packet(buffer, data_size);
-   }
+    if (sock_raw < 0)
+    {
+        perror("Socket Error");
+        return 1;
+    }
 
-   close(sock_raw);
-   free(buffer);
-   return 0;
+    printf("Listening for packets...\n");
+
+    while (1)
+    {
+        int data_size =
+            recvfrom(sock_raw,
+                     buffer,
+                     BUFFER_SIZE,
+                     0,
+                     NULL,
+                     NULL);
+
+        if (data_size < 0)
+        {
+            perror("Recv Error");
+            return 1;
+        }
+
+        packet_count++;
+
+        printf("\nTotal packets received = %lu\n",
+               packet_count);
+
+        inspect_packet(buffer, data_size);
+    }
+
+    close(sock_raw);
+    free(buffer);
+
+    return 0;
 }
